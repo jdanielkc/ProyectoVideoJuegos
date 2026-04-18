725009aba36251d0380f9ae56e85c8ab9d8daa2f
Jose Daniel Garcia Arias <jd.garciaa1@uniandes.edu.co>
2026-04-18 16:58:38 -0500

    Mejorar la lógica de límites del jugador al ajustar la posición manualmente en lugar de usar clamp_ip

--- src/ecs/systems/s_player_bounds.py
+++ src/ecs/systems/s_player_bounds.py
@@ -12,6 +12,12 @@ def system_player_bounds(world: esper.World, screen: pygame.Surface):
 
     for _, (c_t, c_s, _) in components:
         player_rect = c_s.surf.get_rect(topleft=c_t.pos)
-        player_rect.clamp_ip(screen_rect)
-        c_t.pos.x = player_rect.x
-        c_t.pos.y = player_rect.y
+        if player_rect.left < screen_rect.left:
+            c_t.pos.x = screen_rect.left
+        elif player_rect.right > screen_rect.right:
+            c_t.pos.x = screen_rect.right - player_rect.width
+
+        if player_rect.top < screen_rect.top:
+            c_t.pos.y = screen_rect.top
+        elif player_rect.bottom > screen_rect.bottom:
+            c_t.pos.y = screen_rect.bottom - player_rect.height
